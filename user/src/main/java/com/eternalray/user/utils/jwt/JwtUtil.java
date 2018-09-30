b6519ea0bfb568bc9670bf2ad5e6f62c8dc7a648
Eternalray <1339630595@qq.com>
2018-09-30 15:53:46 +0800

    新增JwtUtil工具类,用于后续的SSO

---
+++ user/src/main/java/com/eternalray/user/utils/jwt/JwtUtil.java
@@ -0,0 +1,71 @@
+package com.eternalray.user.utils.jwt;
+
+import com.alibaba.fastjson.JSONObject;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * @Author: Eternalray
+ *  JWT 工具类
+ */
+@Slf4j
+public class JwtUtil {
+
+    //使用项目请更改为您的自定义SECRET
+    private final static String SECRET="YOUR_SECRET";
+
+    /**
+     * 获得JWT token
+     * @param expires       token过期时间(单位：秒)
+     * @param params        附带的其它参数
+     * @return
+     */
+    public static String getToken(Long expires, Map<String,Object> params){
+        String token = null;
+        try {
+            //加密方式
+            Algorithm algorithm = Algorithm.HMAC256(SECRET);
+            System.out.println(algorithm.toString());
+            //过期时间
+            Date expiresDate = new Date(System.currentTimeMillis()+expires*1000);
+            //附带参数的JSON字符串
+            String paramsToJsonStr=JSONObject.toJSONString(params);
+            System.out.println("paramsToJsonStr:"+paramsToJsonStr);
+            token = JWT.create()
+                    .withClaim("params",JSONObject.toJSONString(params))
+                    .withExpiresAt(expiresDate)
+                    .sign(algorithm);
+        } catch (JWTCreationException e){
+            log.debug("获得JWT token 失败！",e.getMessage());
+        } finally {
+            return token;
+        }
+    }
+
+    /**
+     * 验证JWT token
+     * @param token
+     * @return
+     */
+    public static DecodedJWT verifyToken(String token){
+        DecodedJWT jwt = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .build();
+            jwt=verifier.verify(token);
+        } catch (JWTVerificationException e){
+            log.debug("验证JWT token 失败！",e.getMessage());
+        } finally {
+            return jwt;
+        }
+    }
+}
--- user/src/test/java/com/eternalray/user/UserApplicationTests.java
+++ user/src/test/java/com/eternalray/user/UserApplicationTests.java
@@ -1,9 +1,14 @@
 package com.eternalray.user;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.eternalray.user.utils.jwt.JwtUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +18,17 @@ public class UserApplicationTests {
     public void contextLoads() {
     }
 
+    @Test
+    public void jwtUtils() throws InterruptedException {
+        Map<String,Object> map=new HashMap<>();
+        map.put("userName","Eternalray");
+        map.put("userId",1);
+        //生成JWT Token
+        String token = JwtUtil.getToken(10L, map);
+        //验证JWT Token
+        DecodedJWT decodedJWT = JwtUtil.verifyToken(token);
+        //获取并解析JWT Token 附带参数值
+        String payload = decodedJWT.getPayload();
+        System.out.println(payload);
+    }
 }
